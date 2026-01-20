755c0a62da1357e189023f43cc924325cb1cbd33
Martin Güthler <mg@webfactory.de>
2026-01-20 16:16:08 +0100

    Use method `Connection::quoteSingleIdentifier()` only if it exists

--- Storage/DoctrineStorage.php
+++ Storage/DoctrineStorage.php
@@ -53,8 +53,15 @@ class DoctrineStorage implements StorageInterface {
 		$this->storageKeyGenerator = $storageKeyGenerator;
 		// TODO just call `createSchemaManager()` as soon as DBAL >= 3.1 is required
 		$this->schemaManager = \method_exists($this->conn, 'createSchemaManager') ? $this->conn->createSchemaManager() : $this->conn->getSchemaManager();
-		$this->keyColumn = $this->conn->quoteSingleIdentifier(self::KEY_COLUMN);
-		$this->valueColumn = $this->conn->quoteSingleIdentifier(self::VALUE_COLUMN);
+
+        // BC for doctrine/dbal < 4
+        if(method_exists($this->conn, 'quoteSingleIdentifier')) {
+            $this->keyColumn = $this->conn->quoteSingleIdentifier(self::KEY_COLUMN);
+            $this->valueColumn = $this->conn->quoteSingleIdentifier(self::VALUE_COLUMN);
+        } else {
+            $this->keyColumn = $this->conn->quoteIdentifier(self::KEY_COLUMN);
+            $this->valueColumn = $this->conn->quoteIdentifier(self::VALUE_COLUMN);
+        }
 	}
 
 	/**
